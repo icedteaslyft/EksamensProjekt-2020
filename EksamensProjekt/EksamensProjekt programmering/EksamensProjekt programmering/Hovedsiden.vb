22d5a634b276f065795c64a3411662bc3d81479a
Troels Karn <troelskarn@gmail.com>
2020-03-30 15:43:40 +0200

    hovedside kode

--- EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Hovedsiden.vb
+++ EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Hovedsiden.vb
@@ -1,38 +1,55 @@
 ﻿Public Class Hoved_Form
 
 #Region "Knapper"
-    Private Sub Hovedside_But_Click(sender As Object, e As EventArgs)
+
+    Private Sub Hovedside_But_Click_1(sender As Object, e As EventArgs) Handles Hovedside_But.Click
         Me.Hide()
         Me.Show()
-
     End Sub
 
-    Private Sub Tryk_But_Click(sender As Object, e As EventArgs)
+    Private Sub Tyngde_But_Click_1(sender As Object, e As EventArgs) Handles Tyngde_But.Click
         Me.Hide()
-        Tryk_Form_T.Show()
-
+        Tyngdekraft_Form_R.Show()
     End Sub
 
-    Private Sub Varme_But_Click(sender As Object, e As EventArgs)
+    Private Sub Gnidningskraft_But_Click(sender As Object, e As EventArgs) Handles Gnidningskraft_But.Click
         Me.Hide()
-        Varme_Form_T.Show()
-
+        Gnidningskraft_Form_R.Show()
     End Sub
 
-    Private Sub Densitet_But_Click(sender As Object, e As EventArgs)
+    Private Sub Elektrisk_Ladning_But_Click(sender As Object, e As EventArgs) Handles Elektrisk_Ladning_But.Click
+        Me.Hide()
+        Elektrisk_Ladning_Form_R.Show()
+    End Sub
+
+    Private Sub Densitet_But_Click_1(sender As Object, e As EventArgs) Handles Densitet_But.Click
         Me.Hide()
         Densitet_Form_T.Show()
     End Sub
 
-    Private Sub Gnidning_But_Click(sender As Object, e As EventArgs)
+    Private Sub Varme_But_Click_1(sender As Object, e As EventArgs) Handles Varme_But.Click
         Me.Hide()
-        Gnidningskraft_Form_R.Show()
-
+        Varme_Form_T.Show()
     End Sub
 
-    Private Sub Tyngde_But_Click(sender As Object, e As EventArgs)
+    Private Sub Tryk_But_Click_1(sender As Object, e As EventArgs) Handles Tryk_But.Click
         Me.Hide()
-        Tyngdekraft_Form_R.Show()
+        Tryk_Form_T.Show()
+    End Sub
+
+    Private Sub Gravitation_But_Click(sender As Object, e As EventArgs) Handles Gravitation_But.Click
+        Me.Hide()
+        Gravitation_Form_D.Show()
+    End Sub
+
+    Private Sub Idealgasser_But_Click(sender As Object, e As EventArgs) Handles Idealgasser_But.Click
+        Me.Hide()
+        Idealgasser__Form_D.Show()
+    End Sub
+
+    Private Sub Interferens_But_Click(sender As Object, e As EventArgs) Handles Interferens_But.Click
+        Me.Hide()
+        Interferens_Form_D.Show()
     End Sub
 #End Region
 
