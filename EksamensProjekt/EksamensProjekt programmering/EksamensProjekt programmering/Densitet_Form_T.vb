1ce4d078dcfc5e032dc2bb7ecb8f323d9dab314e
Troels Karn <troelskarn@gmail.com>
2020-03-30 16:14:56 +0200

    Tilføjet menuer for Form_T

--- EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Densitet_Form_T.vb
+++ EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Densitet_Form_T.vb
@@ -3,38 +3,55 @@
 
 
 #Region "Knapper"
-    Private Sub Hovedside_But_Click(sender As Object, e As EventArgs) 
+
+    Private Sub Hovedside_But_Click_1(sender As Object, e As EventArgs) Handles Hovedside_But.Click
         Me.Hide()
         Hoved_Form.Show()
-
     End Sub
 
-    Private Sub Tryk_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Tyngde_But_Click_1(sender As Object, e As EventArgs) Handles Tyngde_But.Click
         Me.Hide()
-        Tryk_Form_T.Show()
-
+        Tyngdekraft_Form_R.Show()
     End Sub
 
-    Private Sub Varme_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Gnidningskraft_But_Click(sender As Object, e As EventArgs) Handles Gnidningskraft_But.Click
         Me.Hide()
-        Varme_Form_T.Show()
-
+        Gnidningskraft_Form_R.Show()
     End Sub
 
-    Private Sub Densitet_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Elektrisk_Ladning_But_Click(sender As Object, e As EventArgs) Handles Elektrisk_Ladning_But.Click
+        Me.Hide()
+        Elektrisk_Ladning_Form_R.Show()
+    End Sub
+
+    Private Sub Densitet_But_Click_1(sender As Object, e As EventArgs) Handles Densitet_But.Click
         Me.Hide()
         Me.Show()
     End Sub
 
-    Private Sub Gnidning_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Varme_But_Click_1(sender As Object, e As EventArgs) Handles Varme_But.Click
         Me.Hide()
-        Gnidningskraft_Form_R.Show()
-
+        Varme_Form_T.Show()
     End Sub
 
-    Private Sub Tyngde_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Tryk_But_Click_1(sender As Object, e As EventArgs) Handles Tryk_But.Click
         Me.Hide()
-        Tyngdekraft_Form_R.Show()
+        Tryk_Form_T.Show()
+    End Sub
+
+    Private Sub Gravitation_But_Click(sender As Object, e As EventArgs) Handles Gravitation_But.Click
+        Me.Hide()
+        Gravitation_Form_D.Show()
+    End Sub
+
+    Private Sub Idealgasser_But_Click(sender As Object, e As EventArgs) Handles Idealgasser_But.Click
+        Me.Hide()
+        Idealgasser__Form_D.Show()
+    End Sub
+
+    Private Sub Interferens_But_Click(sender As Object, e As EventArgs) Handles Interferens_But.Click
+        Me.Hide()
+        Interferens_Form_D.Show()
     End Sub
 #End Region
 
@@ -89,6 +106,8 @@
         BeregnDensitetGasser()
         Densitet_Gas_Total_lbl.Text = p_Rho * 1000.ToString() & "Kg/m3"
     End Sub
+
+
 
 #End Region
 
--- EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Varme_Form_T.vb
+++ EksamensProjekt/EksamensProjekt programmering/EksamensProjekt programmering/Varme_Form_T.vb
@@ -3,41 +3,56 @@
 
 
 #Region "Knapper"
-    Private Sub Hovedside_But_Click(sender As Object, e As EventArgs) 
+
+    Private Sub Hovedside_But_Click_1(sender As Object, e As EventArgs) Handles Hovedside_But.Click
         Me.Hide()
         Hoved_Form.Show()
-
     End Sub
 
-    Private Sub Tryk_But_Click(sender As Object, e As EventArgs) 
-        Me.Hide()
-        Tryk_Form_T.Show()
-
-    End Sub
-
-    Private Sub Varme_But_Click(sender As Object, e As EventArgs) 
-        Me.Hide()
-        Me.Show()
-
-    End Sub
-
-    Private Sub Densitet_But_Click(sender As Object, e As EventArgs) 
-        Me.Hide()
-        Densitet_Form_T.Show()
-    End Sub
-
-    Private Sub Gnidning_But_Click(sender As Object, e As EventArgs) 
-        Me.Hide()
-        Gnidningskraft_Form_R.Show()
-
-    End Sub
-
-    Private Sub Tyngde_But_Click(sender As Object, e As EventArgs) 
+    Private Sub Tyngde_But_Click_1(sender As Object, e As EventArgs) Handles Tyngde_But.Click
         Me.Hide()
         Tyngdekraft_Form_R.Show()
     End Sub
 
+    Private Sub Gnidningskraft_But_Click(sender As Object, e As EventArgs) Handles Gnidningskraft_But.Click
+        Me.Hide()
+        Gnidningskraft_Form_R.Show()
+    End Sub
 
+    Private Sub Elektrisk_Ladning_But_Click(sender As Object, e As EventArgs) Handles Elektrisk_Ladning_But.Click
+        Me.Hide()
+        Elektrisk_Ladning_Form_R.Show()
+    End Sub
+
+    Private Sub Densitet_But_Click_1(sender As Object, e As EventArgs) Handles Densitet_But.Click
+        Me.Hide()
+        Densitet_Form_T.Show()
+    End Sub
+
+    Private Sub Varme_But_Click_1(sender As Object, e As EventArgs) Handles Varme_But.Click
+        Me.Hide()
+        Me.Show()
+    End Sub
+
+    Private Sub Tryk_But_Click_1(sender As Object, e As EventArgs) Handles Tryk_But.Click
+        Me.Hide()
+        Tryk_Form_T.Show()
+    End Sub
+
+    Private Sub Gravitation_But_Click(sender As Object, e As EventArgs) Handles Gravitation_But.Click
+        Me.Hide()
+        Gravitation_Form_D.Show()
+    End Sub
+
+    Private Sub Idealgasser_But_Click(sender As Object, e As EventArgs) Handles Idealgasser_But.Click
+        Me.Hide()
+        Idealgasser__Form_D.Show()
+    End Sub
+
+    Private Sub Interferens_But_Click(sender As Object, e As EventArgs) Handles Interferens_But.Click
+        Me.Hide()
+        Interferens_Form_D.Show()
+    End Sub
 #End Region
 
 #Region "Tilført Varme"
@@ -101,6 +116,7 @@
         BeregnSpecifikVarmeKapacitet()
         Specifik_Varmekapacitet_Total_lbl.Text = c_Specifik_Varmekapacitet.ToString & "kJ/(kg*Celsius)"
     End Sub
+
 
 #End Region
 
